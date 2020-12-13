98b8f71ae7a4ac9402f14a180dc42f6def9c3370
Limin Deng <limin.deng@anu.edu.au>
2020-12-14 10:23:44 +1100

    Initial Commit

---
+++ cc/ccApp.swift
@@ -0,0 +1,17 @@
+//
+//  ccApp.swift
+//  cc
+//
+//  Created by dph on 2020/12/14.
+//
+
+import SwiftUI
+
+@main
+struct ccApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
